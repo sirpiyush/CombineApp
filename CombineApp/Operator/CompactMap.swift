c0c2bb6e767bc49188f2182d5f9538dc09f4106b
Dipendra Dubey <dipendradubey@gmail.com>
2024-09-02 12:21:15 +0530

    Worked on operator

---
+++ CombineApp/Operator/CompactMap.swift
@@ -0,0 +1,42 @@
+//
+//  CompactMap.swift
+//  CombineApp
+//
+//  Created by Dipendra Dubey on 02/09/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct CompactMapView:View {
+    @StateObject var viewModel = CompactMapViewModel()
+    var body: some View {
+        List(viewModel.arr, id: \.self){ data in
+            Text(data)
+        }.onAppear{
+            viewModel.loadData()
+        }
+    }
+}
+
+class CompactMapViewModel:ObservableObject{
+    
+    @Published var arr : [String] = []
+    
+    func loadData(){
+        let dict = [1:"One", 2:"Two", 3:"Three", 5:"Five"]
+        let range = 1...5
+        var cancellable =
+        range.publisher
+            .sink {[weak self] element in
+                self?.arr.append(dict[element] ?? "")
+            }
+        
+    }
+    
+}
+
+
+#Preview {
+    CompactMapView()
+}
